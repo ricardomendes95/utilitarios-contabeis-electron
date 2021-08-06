b8c06ff09a34fa279eb03ab0fe034b2049f70df1
ricardomendes95 <sr_ricardomendes@hotmail.com>
2021-08-06 17:28:23 -0300

    feat: get compny

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const cors = require("cors");
 const relatoriosRouter = require("./routes/relatorios");
 
@@ -10,7 +11,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   return res.json("pegou praga");
 });
-app.use("/relatorios", relatoriosRouter);
+
+app.use("/reports", relatoriosRouter);
 
 app.listen(3001, () => console.log("app rodando na porta 3001"));
-// console.log("app rodando na porta 3001");
--- api/src/routes/relatorios.js
+++ api/src/routes/relatorios.js
@@ -1,9 +1,42 @@
 var express = require("express");
+const axios = require("axios");
 var router = express.Router();
 
 /* GET Empresas listing. */
 router.get("/", async function (req, res, next) {
   res.json({ name: "estamos no relatorios" });
+});
+router.post("/responsibilityLetter", async function (req, res, next) {
+  console.log(req.body);
+  var lastUpdateDate = new Date();
+  if (req.body.registrationData === "inicial") {
+    lastUpdateDate = req.body.startDate;
+  } else {
+    lastUpdateDate = req.body.endDate;
+  }
+
+  const crc = req.body.crc === "Do contador" ? 1 : 2;
+
+  const data = await axios.get(
+    "http://localhost:8080/responsabilityLetter?lastUpdateDate=" +
+      lastUpdateDate +
+      "&crcType=" +
+      crc
+  );
+
+  res.json(data.data);
+});
+
+router.get("/companyResponsibilityLetter/:id", async function (req, res, next) {
+  try {
+    const data = await axios.get(
+      "http://localhost:8080/responsabilityLetter/company?id=" + req.params.id
+    );
+
+    return res.json(data.data);
+  } catch (error) {
+    return res.status(500).send("error");
+  }
 });
 
 module.exports = router;
